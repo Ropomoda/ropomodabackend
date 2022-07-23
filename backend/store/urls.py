0428c2b954c55c5e4a3a0f0b9d5a1de5b02171c2
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2022-07-23 21:34:26 +0430

    Add permission classes

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,9 +1,5 @@
-from rest_framework import routers
 from .views import *
 
-router = routers.DefaultRouter()
-router.register(r'product', ProductViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('products/', ProductList.as_view()),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
-
+from rest_framework import viewsets
+from rest_framework.permissions import  IsAuthenticated
+from permissions.permissionClasses import ReadOnly
 from .models import *
 from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
+class ProductList(APIView):
+    # permission_classes = []
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-    
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
