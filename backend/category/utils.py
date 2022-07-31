1017b0241d8f61a283ae25065da0acf80d7608ad
Mohsen Poureiny <mohsenpoureiny@outlook.com>
2022-07-31 12:39:12 +0430

    Fix image path issue in category

--- backend/category/utils.py
+++ backend/category/utils.py
@@ -2,4 +2,4 @@ import uuid
 
 
 def category_image_path(instance , filename):
-    return f'category/images/{instance.uuid}_{uuid.uuid4()}{filename}'
+    return f'category/images/{instance.id}_{uuid.uuid4()}{filename}'
